2d29d555eb524d955191364ffc3c1618b9b17ca9
AEiosApp <ae.iosapp@gmail.com>
2019-11-07 12:07:40 -0500

    Updates

---
+++ precache-manifest.bdf04be30177e7f8a6d7d05c29eb6d8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de604c72200b9418d16f817df0246439",
+    "url": "/my-test-react-app/index.html"
+  },
+  {
+    "revision": "775ea7ce6e8eb4e72a68",
+    "url": "/my-test-react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b0fb120175ca89f844b7",
+    "url": "/my-test-react-app/static/js/2.b898528c.chunk.js"
+  },
+  {
+    "revision": "775ea7ce6e8eb4e72a68",
+    "url": "/my-test-react-app/static/js/main.e83c98ee.chunk.js"
+  },
+  {
+    "revision": "40e6a3f981ccb89e9b61",
+    "url": "/my-test-react-app/static/js/runtime-main.c5224c9d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-test-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
